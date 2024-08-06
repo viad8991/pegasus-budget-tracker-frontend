85e5965c9fbeb8448c9e2379d1c97daad0ed31b1
viad8991 <viad8991@yandex.ru>
2024-08-06 20:01:09 +0000

    save

--- src/axios.ts
+++ src/axios.ts
@@ -36,3 +36,68 @@ axios.interceptors.response.use(
 );
 
 export default axios;
+
+/* SAVE:
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+
+class ApiClient {
+  private axiosInstance: AxiosInstance;
+
+  constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: "http://localhost:8080/",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    // Добавление интерсепторов
+    this.axiosInstance.interceptors.request.use(
+      (config: AxiosRequestConfig) => {
+        const token = localStorage.getItem("token"); // Можно заменить на вызов из store
+        if (token) {
+          config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+      },
+      (error) => {
+        return Promise.reject(error);
+      }
+    );
+
+    this.axiosInstance.interceptors.response.use(
+      (response: AxiosResponse) => response,
+      (error) => {
+        if (error.response?.status === 401) {
+          // Логика при неавторизованном доступе
+        } else if (error.response?.status === 403) {
+          // Логика при доступе запрещена
+        }
+        return Promise.reject(error);
+      }
+    );
+  }
+
+  public async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const response = await this.axiosInstance.get<T>(url, config);
+    return response.data;
+  }
+
+  public async post<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T> {
+    const response = await this.axiosInstance.post<T>(url, data, config);
+    return response.data;
+  }
+
+  public async put<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T> {
+    const response = await this.axiosInstance.put<T>(url, data, config);
+    return response.data;
+  }
+
+  public async delete<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const response = await this.axiosInstance.delete<T>(url, config);
+    return response.data;
+  }
+}
+
+export default new ApiClient();
+*/
