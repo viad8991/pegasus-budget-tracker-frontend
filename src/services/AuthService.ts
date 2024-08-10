82c5d77acda6e934fae6633e3f7177018f2d39a3
viad8991 <viad8991@yandex.ru>
2024-08-10 08:07:39 +0000

    save

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,7 +1,20 @@
-import {LoginResponse} from "../store/auth/authTypes";
+import { LoginResponse } from "../store/auth/authTypes";
 import axios from "../utils/axios";
 
 class AuthService {
+    private stub: LoginResponse = {
+        token: "stub",
+        user: {
+            id: "4cfd7412-eafe-4056-a5ec-343a3a662c3d",
+            hasFamily: true,
+            username: "admin",
+            isAdmin: true,
+            email: "admin@admin.com",
+            verified: true,
+            isActive: true,
+        },
+    };
+
     async fetchUserData(token: string) {
         // token
         return await axios
@@ -11,25 +24,13 @@ class AuthService {
             })
             .catch((reason) => {
                 console.log(reason);
-                const stub: LoginResponse = {
-                    token: "stub",
-                    user: {
-                        id: "4cfd7412-eafe-4056-a5ec-343a3a662c3d",
-                        hasFamily: true,
-                        username: "admin",
-                        isAdmin: true,
-                        email: "admin@admin.com",
-                        verified: true,
-                        isActive: true,
-                    },
-                };
-                return stub;
+                return this.stub;
             });
     }
 
     async login(username: string, password: string) {
         return await axios
-            .post<LoginResponse>("/api/v1/auth", {username, password})
+            .post<LoginResponse>("/api/v1/auth", { username, password })
             .then((response) => {
                 return response.data;
             })
